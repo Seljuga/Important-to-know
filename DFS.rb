6a0bf68e68dce1f8911a958b042b0797a5d618a3
Seljuga <domagoj.simag@gmail.com>
2024-12-08 09:52:27 +0100

    Update DFS.rb

--- DFS.rb
+++ DFS.rb
@@ -8,6 +8,12 @@
 
 # DFS from a Given Source of Undirected Graph:
 
+
+
+# The algorithm starts from a given source and explores all reachable vertices from the given source. 
+# It is similar to Preorder Tree Traversal where we visit the root, then recur for its children. 
+# In a graph, there maybe loops. So we use an extra visited array to make sure that we do not process a vertex again.
+
 include <bits/stdc++.h>
 using namespace std;
 
@@ -49,6 +55,74 @@ int main(){
     int s = 1; // Starting vertex for DFS
     cout << "DFS from source: " << s << endl;
     DFS(adj, s); // Perform DFS starting from the source vertex
+
+    return 0;
+}
+
+
+# Time complexity: O(V + E), where V is the number of vertices and E is the number of edges in the graph.
+# Auxiliary Space: O(V + E), since an extra visited array of size V is required, And stack size for recursive calls to DFSRec function.
+
+
+
+# DFS for Complete Traversal of Disconnected Undirected Graph
+
+# The above implementation takes a source as an input and prints only those vertices that are reachable from the source and would not
+# print all vertices in case of disconnected graph.
+# Let us now talk about the algorithm that prints all vertices without any source and the graph maybe disconnected.
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void addEdge(vector<vector<int>> &adj, int s, int t){
+    adj[s].push_back(t);
+    adj[t].push_back(s);
+}
+
+# Recursive function for DFS traversal
+void DFSRec(vector<vector<int>> &adj, vector<bool> &visited,int s){
+    # Mark the current vertex as visited
+    visited[s] = true;
+
+    # Print the current vertex
+    cout << s << " ";
+
+    # Recursively visit all adjacent vertices that are not visited yet
+    for (int i : adj[s])
+        if (visited[i] == false)
+            DFSRec(adj, visited, i);
+}
+
+# Main DFS function to perform DFS for the entire graph
+void DFS(vector<vector<int>> &adj){
+    vector<bool> visited(adj.size(), false);
+
+    # Loop through all vertices to handle disconnected graph
+    for (int i = 0; i < adj.size(); i++){
+        if (visited[i] == false){
+            # If vertex i has not been visited, perform DFS from it
+            DFSRec(adj, visited, i);
+        }
+    }
+}
+
+int main(){
+    int V = 6;
+    # Create an adjacency list for the graph
+    vector<vector<int>> adj(V);
+
+    # Define the edges of the graph
+    vector<vector<int>> edges = {{1, 2}, {2, 0}, {0, 3}, {4, 5}};
+
+    # Populate the adjacency list with edges
+    for (auto &e : edges)
+        addEdge(adj, e[0], e[1]);
+
+    cout << "Complete DFS of the graph:" << endl;
+    DFS(adj);
 
     return 0;
 }
